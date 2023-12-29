212d9e0a112d1a920da34c4dad6558e509eb8887
orlanddoneto <neto020735@gmail.com>
2023-12-29 14:13:45 -0300

    Primeira versão do sistema

--- functions.py
+++ functions.py
@@ -3,13 +3,14 @@ from collections import defaultdict
 import requests
 from datetime import datetime
 #[0] uuid || [1] capabilities
-tabela_resource= defaultdict(list)
+
 
 def initialize_tabela_resources():
     """Preenche a tabela_resources"""
 
     url = "http://cidadesinteligentes.lsdi.ufma.br/inc/2023/collector/resources/data"
     try:
+        tabela_resource= defaultdict(list)
         response = requests.get(url)
         json_response = response.json()
         for resource in json_response["resources"]:
@@ -18,48 +19,60 @@ def initialize_tabela_resources():
           try:
             response_uid = requests.get(url_uuid)
             dados_carro = response_uid.json()["data"]["description"].split(";")
-            placa = dados_carro[2]
-            tabela_resource[placa].append(url_uuid)
-            tabela_resource[placa].append(resource["capabilities"])
-          except Exception as e:
-            print(f"Ocorreu um erro: {e}")
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
 
-def get_dados_carro(placa):
+            if (len(dados_carro)>=3):
+                placa = dados_carro[2]
+                tabela_resource[placa].append(url_uuid)
+                tabela_resource[placa].append(resource["capabilities"])
+            else:
+                continue
+          except Exception as e:
+            print(f"Ocorreu um erro na initialize: {e}")
+        return tabela_resource
+    except Exception as e:
+        print(f"Ocorreu um erro na initialize: {e}")
+
+def get_dados_carro(placa, tabela_resource):
     """Retorna um dicionário com os dados de um carro"""
 
     try:
         response_uid = requests.get(tabela_resource[placa][0])
         dados_carro = response_uid.json()["data"]["description"].split(";")
-        table_dados = {}
-        table_dados['placa'] = dados_carro[2]
-        table_dados['marca'] = dados_carro[3]
-        table_dados['modelo'] = dados_carro[4]
-        table_dados['ano'] = dados_carro[5]
-        return table_dados
+        if(len(dados_carro)>=6):
+            table_dados = {}
+            table_dados['placa'] = dados_carro[2]
+            table_dados['marca'] = dados_carro[3]
+            table_dados['modelo'] = dados_carro[4]
+            table_dados['ano'] = dados_carro[5]
+            return table_dados
+        else:
+            return []
 
     except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-    return False
+        print(f"Ocorreu um erro na get dados carro: {e}")
 
-def get_localizacao(placa):
+def get_localizacao(placa, tabela_resource):
     """Retorna um dicionario com informações de localização e horário"""
     try:
-        capacidades = tabela_resource[placa][1]
+        capacidades = tabela_resource.get(placa, [])[1]
         localizacao = []
         i = 0
         if "localizacao" in capacidades:
             for location in capacidades["localizacao"]:
-                localizacao[i][0] = location["lat"]
-                localizacao[i][1] = location["lon"]
+                lat = location["lat"]
+                lon = location["lon"]
                 data_obj = datetime.strptime(location["date"], "%Y-%m-%dT%H:%M:%S.%fZ")
-                localizacao[i][2] = data_obj.strftime("%Y-%m-%d")
-                localizacao[i][3] = data_obj.strftime("%H:%M:%S.%f")[:-3]
-                i+=i
+                data_formatada = data_obj.strftime("%Y-%m-%d")
+                hora_formatada = data_obj.strftime("%H:%M:%S.%f")[:-3]
+                
+                # Adicionando uma nova lista de localização à lista principal
+                localizacao.append([lat, lon, data_formatada, hora_formatada])
             return localizacao
+        else:
+            return []
     except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-    return False
+        print(f"Ocorreu um erro na getlocalizao: {e}")
+        return []
+    
 
 
--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import requests
 import functions as func
 import folium
 
-func.initialize_tabela_resources
+tabela_resource = func.initialize_tabela_resources()
 
 app = Flask(__name__)
 
@@ -26,21 +26,28 @@ def rastrear():
     placa = request.args.get("placa")
     vehicle_rows = ""
     try:
-        dados_carro = func.get_dados_carro(placa)    
-        localizacao = func.get_localizacao(placa)
-        
+        dados_carro = func.get_dados_carro(placa,tabela_resource)    
+        localizacao = func.get_localizacao(placa,tabela_resource)
         #Quando voltar o servidor
         #substituir o parâmetro location por:
         #localizacao[0][0], localizacao[0][1]
-        m = folium.Map(location=[-2.559734, -44.309316], zoom_start=13)
-        """
-        for pos in localizacao:
-            lat = pos[0]
-            lon = pos[1]
-            data = pos[2]
-            hora = pos[3]
-            folium.Marker([lat, lon], popup=f"Visto em {data}, às {hora}").add_to(m)
-        """
+        i = 0
+        while(True):
+            if localizacao and localizacao[i]:
+                m = folium.Map(location=[localizacao[i][0], localizacao[i][1]], zoom_start=13)
+                break
+            elif(len(localizacao)-1 == i):
+                break
+            i+=1
+
+        if(localizacao):
+            for pos in localizacao:
+                lat = pos[0]
+                lon = pos[1]
+                data = pos[2]
+                hora = pos[3]
+                folium.Marker([lat, lon], popup=f"Visto em {data}, às {hora}").add_to(m)
+
         map_html = m._repr_html_()
 
         if dados_carro:
@@ -49,6 +56,7 @@ def rastrear():
             return render_template("rastreio.html", vehicle_rows=[],map_html=map_html)
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
+        return render_template("erro.html",error_message=str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
